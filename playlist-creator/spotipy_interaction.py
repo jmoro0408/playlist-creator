f6f97ff2a59b1e386431985d4e17d64599610dc5
jmoro0408 <james.moro.11@gmail.com>
2022-09-04 18:02:26 +0100

    initial commit

---
+++ playlist-creator/spotipy_interaction.py
@@ -0,0 +1,34 @@
+import configparser
+import spotipy
+from spotipy.oauth2 import SpotifyOAuth
+from pathlib import Path
+
+
+def import_config() -> tuple:
+    config = configparser.ConfigParser()
+    root_dir = Path(__file__).resolve().parents[0]
+    config.read(Path(root_dir, "config.ini"))
+    _client_id = config["SPOTIFY"]["SPOTIPY_CLIENT_ID"]
+    _client_secret = config["SPOTIFY"]["SPOTIPY_CLIENT_SECRET"]
+    return _client_id, _client_secret
+
+
+class SpotipyClient(object):
+    def __init__(self, client_id: str, client_secret: str):
+        self.client_id = client_id
+        self.client_secret = client_secret
+        self.authorize = self.spotify_auth()
+
+    def spotify_auth(self):
+        redirect_uri = "http://localhost:8888/callback"
+        scope = "user-library-read"
+        return spotipy.Spotify(auth_manager=SpotifyOAuth(scope=scope,
+                client_id=self.client_id,
+                client_secret=self.client_secret,
+                redirect_uri=redirect_uri))
+
+if __name__== "__main__":
+    pass
+
+
+
---
+++ playlist-creator/tests/test_spotipy_interaction.py
@@ -0,0 +1,22 @@
+from spotipy_interaction import SpotipyClient
+from pathlib import Path
+import configparser
+import pytest
+
+@pytest.fixture
+def client_fixture():
+    config = configparser.ConfigParser()
+    root_dir = Path(__file__).resolve().parents[1]
+    config.read(Path(root_dir, "config.ini"))
+    _client_id = config["SPOTIFY"]["SPOTIPY_CLIENT_ID"]
+    _client_secret = config["SPOTIFY"]["SPOTIPY_CLIENT_SECRET"]
+    return SpotipyClient(client_id=_client_id, client_secret=_client_secret).authorize
+
+
+def test_spotipy_login(client_fixture):
+    artist= 'The Beatles'
+    track= 'Yellow Submarine'
+    track_id = client_fixture.search(q='artist:' + artist + ' track:' + track, type='track')
+    track_id = track_id['tracks']['items'][0]['id']
+    track_json = (client_fixture.tracks([track_id]))
+    assert track_json["tracks"][0]["artists"][0]["name"] == artist
