5b2b76d9b0a0d49fb9386a433191e4bea1f28cec
jmoro0408 <james.moro.11@gmail.com>
2022-09-06 16:52:37 +0100

    add build_song_df func and tests

--- playlist-creator/build_song_df.py
+++ playlist-creator/build_song_df.py
@@ -2,9 +2,9 @@ import pandas as pd
 from spotipy_interaction import SpotipyClient, import_config
 
 
-def build_liked_song_df(sp: SpotipyClient) -> pd.DataFrame:
+def build_liked_song_df(sp: SpotipyClient, limit: int = 10) -> pd.DataFrame:
     """Return a dataframe with the users liked tracks,
-    with artist, duration, and audio features for each track
+    with track name, artist, duration, and audio features for each track
 
     Args:
         sp (SpotipyClient): authorised spotipy client object
@@ -12,13 +12,24 @@ def build_liked_song_df(sp: SpotipyClient) -> pd.DataFrame:
     Returns:
         pd.DataFrame: dataframe with track details
     """
-    liked_tracks_json = sp.get_users_liked_tracks()
-    return sp.parse_users_liked_tracks(liked_tracks_json)
+    liked_tracks_json = sp.get_users_liked_tracks(limit=limit)
+    # build df for one song first
+    parsed_liked_tracks_json = sp.parse_users_liked_tracks(liked_tracks_json)
+    # get audio features for tracks
+    track_names = [x[0] for x in parsed_liked_tracks_json]
+    artist_names = [x[1] for x in parsed_liked_tracks_json]
+    track_ids = [
+        x[2] for x in parsed_liked_tracks_json
+    ]  # get the ID part of the parsed json tracks only
+    audio_features = sp.get_track_features(track_ids)
+    liked_song_df = pd.DataFrame(audio_features)
+    liked_song_df["artist_names"] = artist_names
+    liked_song_df["track_names"] = track_names
+    return liked_song_df
 
 
 if __name__ == "__main__":
     client_id, client_secret = import_config()
     sp = SpotipyClient(client_id, client_secret)
-
-    liked_songs_test = build_liked_song_df(sp)
-    print(liked_songs_test)
+    liked_songs_df = build_liked_song_df(sp, 20)
+    print(liked_songs_df.head())
---
+++ playlist-creator/tests/test_build_song_df.py
@@ -0,0 +1,56 @@
+import configparser
+from pathlib import Path
+
+import pytest
+from spotipy_interaction import SpotipyClient
+from build_song_df import build_liked_song_df
+import pandas as pd
+
+
+@pytest.fixture
+def client_fixture():
+    config = configparser.ConfigParser()
+    root_dir = Path(__file__).resolve().parents[1]
+    config.read(Path(root_dir, "config.ini"))
+    _client_id = config["SPOTIFY"]["SPOTIPY_CLIENT_ID"]
+    _client_secret = config["SPOTIFY"]["SPOTIPY_CLIENT_SECRET"]
+    return SpotipyClient(client_id=_client_id, client_secret=_client_secret)
+
+
+def test_build_liked_song_df(client_fixture):
+    results_df = build_liked_song_df(client_fixture)
+    errors = []
+    if not isinstance(results_df, pd.DataFrame):
+        _msg = f"{type(results_df)} returned, expected Dataframe"
+        errors.append(_msg)
+    expected_columns = [
+        "danceability",
+        "energy",
+        "key",
+        "loudness",
+        "mode",
+        "speechiness",
+        "acousticness",
+        "instrumentalness",
+        "liveness",
+        "valence",
+        "tempo",
+        "type",
+        "id",
+        "uri",
+        "track_href",
+        "analysis_url",
+        "duration_ms",
+        "time_signature",
+        "artist_names",
+        "track_names",
+    ]
+    missing_columns = [x for x in expected_columns if x not in results_df.columns]
+    extra_columns = [x for x in results_df.columns if x not in expected_columns]
+    if len(missing_columns) != 0:
+        _msg = f"{missing_columns} columns not found in dataframe"
+        errors.append(_msg)
+    if len(extra_columns) != 0:
+        _msg = f"{extra_columns} columns were found in the dataframe that were not expected"
+        errors.append(_msg)
+    assert len(errors) == 0
