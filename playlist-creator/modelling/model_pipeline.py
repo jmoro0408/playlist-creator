fa2d1947f375767cac2b5d8565de8391a0502b4a
jmoro0408 <james.moro.11@gmail.com>
2022-09-17 17:20:32 +0200

    moved prep_df func to utils module

--- playlist-creator/modelling/model_pipeline.py
+++ playlist-creator/modelling/model_pipeline.py
@@ -4,19 +4,37 @@ from imblearn.pipeline import Pipeline as imbPipeline
 from sklearn.compose import ColumnTransformer
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.preprocessing import OneHotEncoder, StandardScaler
+from sklearn.base import BaseEstimator
+from sklearn.linear_model import SGDClassifier
+class ClfSwitcher(BaseEstimator):
+
+    def __init__(
+        self,
+        estimator = SGDClassifier(),
+    ):
+        """
+        A Custom BaseEstimator that can switch between classifiers.
+        :param estimator: sklearn object - The classifier
+        """
+
+        self.estimator = estimator
+
+    def fit(self, X, y=None, **kwargs):
+        self.estimator.fit(X, y)
+        return self
+
+    def predict(self, X, y=None):
+        return self.estimator.predict(X)
+
+    def predict_proba(self, X):
+        return self.estimator.predict_proba(X)
+
+    def score(self, X, y):
+        return self.estimator.score(X, y)
 
 
-def prep_playlist_df():
-    playlist_dir = r"/Users/jamesmoro/Documents/Python/playlist-recommender/playlist-creator/data/playlist_df.pkl"
-    df = pd.read_pickle(playlist_dir)
-    useless_cols = ["type", "id", "uri", "track_href", "analysis_url"]
-    df = df.drop(useless_cols, axis=1)
-    X = df.drop("playlist_name", axis=1)
-    y = df["playlist_name"]
-    return X, y
 
-
-def make_model_pipeline(X):
+def make_random_forest_pipeline(X):
     cat_features = X.select_dtypes(include=["object"]).columns.to_list()
     num_features = [x for x in X.columns if x not in cat_features]
     numeric_transformer = StandardScaler()
@@ -38,6 +56,26 @@ def make_model_pipeline(X):
     )
     return pipeline
 
+def make_model_pipeline(X):
+    cat_features = X.select_dtypes(include=["object"]).columns.to_list()
+    num_features = [x for x in X.columns if x not in cat_features]
+    numeric_transformer = StandardScaler()
+    categorical_transformer = OneHotEncoder(handle_unknown="ignore")
+    oversampler = RandomOverSampler()
+    featurisation = ColumnTransformer(
+        transformers=[
+            ("num", numeric_transformer, num_features),
+            ("cat", categorical_transformer, cat_features),
+        ]
+    )
+    pipeline = imbPipeline(
+        [
+            ("features", featurisation),
+            ("sampler", oversampler),
+            ('clf', ClfSwitcher()),
+        ]
+    )
+    return pipeline
 
 if __name__ == "__main__":
     pass
