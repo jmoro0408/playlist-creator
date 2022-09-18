570206da8be97d7394b55efb23d36334ded522cd
jmoro0408 <james.moro.11@gmail.com>
2022-09-18 21:27:48 +0100

    removed track_names from training data

--- playlist-creator/modelling/utils.py
+++ playlist-creator/modelling/utils.py
@@ -3,7 +3,7 @@ import pandas as pd
 def prep_playlist_df():
     playlist_dir = r"/Users/jamesmoro/Documents/Python/playlist-recommender/playlist-creator/data/playlist_df.pkl"
     df = pd.read_pickle(playlist_dir)
-    useless_cols = ["type", "id", "uri", "track_href", "analysis_url"]
+    useless_cols = ["type", "id", "uri", "track_href", "analysis_url", "track_names"]
     df = df.drop(useless_cols, axis=1)
     X = df.drop("playlist_name", axis=1)
     y = df["playlist_name"]
