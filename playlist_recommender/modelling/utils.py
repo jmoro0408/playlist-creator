5190d7450a5fdfc1b41228efd0a799463ce01772
jmoro0408 <james.moro.11@gmail.com>
2022-09-24 09:29:18 +0100

    update root folder name

--- playlist_recommender/modelling/utils.py
+++ playlist_recommender/modelling/utils.py
@@ -2,7 +2,7 @@ import pandas as pd
 
 
 def prep_playlist_df():
-    playlist_dir = r"/Users/jamesmoro/Documents/Python/playlist-recommender/playlist-creator/data/playlist_df.pkl"
+    playlist_dir = r"/Users/jamesmoro/Documents/Python/playlist-recommender/playlist_recommender/data/playlist_df.pkl"
     df = pd.read_pickle(playlist_dir)
     useless_cols = ["type", "id", "uri", "track_href", "analysis_url", "track_names"]
     df = df.drop(useless_cols, axis=1)
